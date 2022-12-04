63efa48527a0953505a016dbfe065fbeea42ee33
TanPuc <htanphuc1212@gmail.com>
2022-12-04 22:20:25 +0700

    a

--- investeen/src/App.js
+++ investeen/src/App.js
@@ -4,8 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="App-NavigationBar"></div>
-      <header className="App-header">
+      <div className="App-NavigationBar">
+        <a className="App-Logo">Investeen</a>
+        <div className="App-NavigationBarTopRight">
+          <a className="App-FinancialTest">Kiểm tra</a>
+          <a className="App-FinancialPlan">Kế hoạch chi tiêu</a>
+          <a className="App-Investment">Đầu tư</a>
+        </div>
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +25,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
