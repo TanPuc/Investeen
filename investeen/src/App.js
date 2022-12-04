61a606eff4f856f5c9b0f9d4ecbee2a677bcadc1
TanPuc <htanphuc1212@gmail.com>
2022-12-05 00:04:47 +0700

    a

--- investeen/src/App.js
+++ investeen/src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="App-NavigationBar">
-        <a className="App-Logo">Investeen</a>
+        <p className="App-Logo">Investeen</p>
         <div className="App-NavigationBarTopRight">
           <a className="App-FinancialTest">Kiểm tra</a>
           <a className="App-FinancialPlan">Kế hoạch chi tiêu</a>
           <a className="App-Investment">Đầu tư</a>
         </div>
+        <a className="App-UserLogo"></a>
       </div>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
