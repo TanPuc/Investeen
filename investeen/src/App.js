e1a4119a00ef2633852aa08e5aca62dab08d598b
Harlerina <110514510+Harlerina@users.noreply.github.com>
2022-12-06 22:42:09 +0700

    homepage changes
    add text, button & picture

--- investeen/src/App.js
+++ investeen/src/App.js
@@ -16,8 +16,22 @@ function App() {
 
       <div className='home-first-section'> 
       <h1 className='first-section'>Investeen</h1>
-      <h2 className='second-section'>Hiểu được rõ hơn về xu hướng chi tiêu của bản thân. Lên kế hoạch tiết kiệm cho những mục tiêu tương lai & Xây dựng nền tảng về kiến thức tài chính</h2>
+      <h2 className='second-section'>Hiểu được rõ hơn về xu hướng chi tiêu của bản thân. Lên kế hoạch tiết kiệm cho những mục tiêu tương lai & Xây dựng nền tảng về kiến thức tài chính.</h2>     
+      
       </div>
+
+      <div className= 'test-button-ct'>  
+      <button className='test-button' onClick='BẮT ĐẦU BÀI TEST'>
+      <p className='start-test-button-text'>BẮT ĐẦU BÀI TEST</p>
+</button> 
+      </div>
+      
+      <div className='home-page-pic'>
+      <img className='home-page-pic1' src={require('./Assets/homepagepic1.jpg')}></img>
+      </div>
+      {/* <div className= 'home-page-pic1'> 
+      <img className='User' src={require('./Assets/homepagepic1.jpg')}></img>
+      </div> */}
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
