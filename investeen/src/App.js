890cba3a02b2b85d10246a562e83743743a64a88
Harlerina <110514510+Harlerina@users.noreply.github.com>
2022-12-06 21:46:06 +0700

    home page 1
    headline and description of the website

--- investeen/src/App.js
+++ investeen/src/App.js
@@ -7,11 +7,16 @@ function App() {
       <div className="App-NavigationBar">
         <p className="App-Logo"><img className='Logo' src={require('./logo.png')}></img></p>
         <div className="App-NavigationBarTopRight">
-          <a className="App-NavBox">Kiểm tra</a>
+          <a className="App-NavBox">Trang chủ</a>
           <a className="App-NavBox">Kế hoạch chi tiêu</a>
           <a className="App-NavBox">Đầu tư</a>
         </div>
-        <a className="App-UserLogo"><img className='User' src={require('./Assets/Avatar.png')}></img></a>
+        <a className="App-UserLogo"><img className='User' src={require('./Assets/userAvatar.png')}></img></a>
+      </div>
+
+      <div className='home-first-section'> 
+      <h1 className='first-section'>Investeen</h1>
+      <h2 className='second-section'>Hiểu được rõ hơn về xu hướng chi tiêu của bản thân. Lên kế hoạch tiết kiệm cho những mục tiêu tương lai & Xây dựng nền tảng về kiến thức tài chính</h2>
       </div>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -28,7 +33,9 @@ function App() {
         </a>
       </header> */}
     </div>
+    
   );
+  
 }
 
 export default App;
